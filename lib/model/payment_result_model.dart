3b1f192bf0b57c288c78fac68b82c611365a4ce2
xujun <443798536@qq.com>
2021-08-27 11:26:30 +0800

    update file

--- lib/model/payment_result_model.dart
+++ lib/model/payment_result_model.dart
@@ -8,10 +8,10 @@ import 'package:flutter_union_pay/enum/union_pay_enum.dart';
 // 描述：
 
 class PaymentResult {
-  int code = 0;
-  PaymentStatus get status => PaymentStatus.values[code];
-  PaymentResult.fromJson(String e) {
-    dynamic json = jsonDecode(e);
+  int? code = 0;
+  PaymentStatus get status => PaymentStatus.values[code!];
+  PaymentResult.fromJson(String? e) {
+    dynamic json = jsonDecode(e!);
     this.code = json['code'];
   }
 }
